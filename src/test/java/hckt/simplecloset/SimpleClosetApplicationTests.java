f69b1202af49d7ada4f7ba885a9714f76afa59bb
LeeJuho <ailiartsual2@gmail.com>
2024-05-03 14:56:56 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/hckt/simplecloset/SimpleClosetApplicationTests.java
@@ -0,0 +1,13 @@
+package hckt.simplecloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleClosetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
