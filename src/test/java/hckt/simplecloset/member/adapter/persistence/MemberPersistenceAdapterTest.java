b5d1e086324f2c42d3598d49ddae7fa3ef747ad8
ailiartsua <ailiartsual2@gmail.com>
2024-05-07 17:54:29 +0900

    feat : 회원 도메인 추가 #1 (8 / 8)
    - 회원 어댑터 테스트 코드 추가

---
+++ src/test/java/hckt/simplecloset/member/adapter/persistence/MemberPersistenceAdapterTest.java
@@ -0,0 +1,73 @@
+package hckt.simplecloset.member.adapter.persistence;
+
+import hckt.simplecloset.member.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class MemberPersistenceAdapterTest {
+    private static final String EMAIL = "test@example.com";
+    private static final String PASSWORD = "test";
+    @Autowired
+    MemberPersistenceAdapter memberPersistenceAdapter;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Nested
+    @DisplayName("회원 저장 테스트")
+    class Save {
+        @Test
+        @DisplayName("회원 저장시 ID가 생성된다.")
+        void test1() {
+            // given
+            Member member = new Member(EMAIL, PASSWORD);
+
+            // when
+            memberPersistenceAdapter.save(member);
+
+            // then
+            assertThat(member.getId()).isNotNull();
+        }
+    }
+
+    @Nested
+    @DisplayName("이메일로 회원 조회 테스트")
+    class FindByEmail {
+        @Test
+        @DisplayName("회원 미생성후 조회시 Optional.empty()가 true이다.")
+        void test1() {
+            // given & when
+            Optional<Member> member = memberPersistenceAdapter.findByEmail(EMAIL);
+
+            // then
+            assertThat(member.isEmpty()).isTrue();
+        }
+
+        @Test
+        @DisplayName("회원 생성후 조회시 Optional.empty()가 false이다.")
+        void test2() {
+            // given
+            Member member = new Member(EMAIL, PASSWORD);
+            memberRepository.save(member);
+
+            // when
+            Optional<Member> foundMember = memberPersistenceAdapter.findByEmail(EMAIL);
+
+            // then
+            assertThat(foundMember.isEmpty()).isFalse();
+        }
+    }
+}
