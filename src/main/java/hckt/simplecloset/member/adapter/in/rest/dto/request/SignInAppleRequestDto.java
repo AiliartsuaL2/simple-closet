aac4f3d992420afa53bf3e76c321cd81d63879f3
ailiartsua <ailiartsual2@gmail.com>
2024-05-29 18:19:38 +0900

    fix: oauth _ apple 로그인 버그 수정 _ 토큰 매핑 설정

--- src/main/java/hckt/simplecloset/member/adapter/in/rest/dto/request/SignInAppleRequestDto.java
+++ src/main/java/hckt/simplecloset/member/adapter/in/rest/dto/request/SignInAppleRequestDto.java
@@ -1,10 +1,11 @@
 package hckt.simplecloset.member.adapter.in.rest.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class SignInAppleRequestDto {
     private String code;
     private String id_token;
